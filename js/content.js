cf4154ef9f50b769aaa5428e819f1e074ae16b2b
Biboswan Roy <biboswan98@gmail.com>
2023-01-04 18:54:24 +0530

    Add radio button for color type and initial imple of play and reset

--- js/content.js
+++ js/content.js
@@ -1,22 +1,68 @@
 let _selectedElement;
-const A11Y_CONTEXT_CONTENT = 'A11Y_CONTEXT_CONTENT';
+let _markerGap='15';
+let _colorContrastType='text-color';
+let contrastColor;
+const COLOR_TRANSPARENT = ['rgba(0,0,0,0)','transparent'];
+let canvas;
+
+let shouldRunContrast=0;
+
 const A11Y_CONTEXT = 'A11Y_CONTEXT';
 const PANEL_INIT = `${A11Y_CONTEXT}_PANEL_INIT`;
 const PANEL_REGISTER_FRAME = `${A11Y_CONTEXT}_REGISTER_FRAME`;
 const UPDATE_SELECTED_ELEMENT = `${A11Y_CONTEXT}_UPDATE_SELECTED_ELEMENT`;
+const SET_COLOR_CONTRAST = `${A11Y_CONTEXT}_SET_COLOR_CONTRAST`;
+const UPDATE_CONTRAST_COLOR = `${A11Y_CONTEXT}_UPDATE_CONTRAST_COLOR`;
+const SET_MARKER_GAP = `${A11Y_CONTEXT}_SET_MARKER_GAP`;
+const UPDATE_SHOULD_RUN_CONTRAST = `${A11Y_CONTEXT}_UPDATE_SHOULD_RUN_CONTRAST`;
 
-//const port = chrome.runtime.connect( { name: A11Y_CONTEXT_CONTENT } );
+const colorTypeToStyle = {
+	'text-color':'color',
+	'text-border': '-webkit-text-stroke-color',
+	'border-color': 'border-color'
+};
 
-function createMessage(element) {
-    console.log('elem',element);
-    console.log('elem:',element.nodeType);
+function colorForElement(element, what) {
+    let color = null;
+    let w = element.ownerDocument.defaultView;
+    while (element) {
+        color = w.getComputedStyle(element).getPropertyValue(what);
+        if (color && !COLOR_TRANSPARENT.includes(color)) {
+            break;
+        }
+        element = element.parentElement;
+    }
+    return color;
+}
+
+//Find the nearest parent element which decides its background color or background-image
+function findBgElement(element) {
+	let color = null;
+    let w = element.ownerDocument.defaultView;
+    while (element) {
+        color = w.getComputedStyle(element).getPropertyValue('background-color');
+        if (color && !COLOR_TRANSPARENT.includes(color)) {
+            return { element, backgroundColor: color }
+        }
+
+		color = w.getComputedStyle(element).getPropertyValue('background-image');
+		if (color!=='none') {
+			return { element, backgroundImage: color }
+		}
+        element = element.parentElement;
+    }
+    return null;
+}
+
+function createMessageForSelectedElement(element) {
     if (!element || element.nodeType !== 1 ) {
 		return { type: UPDATE_SELECTED_ELEMENT, selectedElement: {} };
 	}
-    console.log('elem:',element);
+
 	return {
         type: UPDATE_SELECTED_ELEMENT,
         selectedElement: element.outerHTML,
+		color: contrastColor
     };
 }
 
@@ -24,12 +70,86 @@ function setSelectedElement(element) {
     // If the selected element is the same, let handlers in other iframe contexts handle it instead.
 	if (_selectedElement === undefined || element !== _selectedElement) {
 		_selectedElement = element;
-		chrome.runtime.sendMessage(createMessage(element));
+		contrastColor = colorForElement(_selectedElement,colorTypeToStyle[_colorContrastType]);
+		chrome.runtime.sendMessage(createMessageForSelectedElement(element));
 	}
 }
 
-chrome.runtime.onMessage.addListener(function ( message ) {
-	if ( message.type === PANEL_INIT) {
-		chrome.runtime.sendMessage( { type: PANEL_REGISTER_FRAME, url: window.location.href } );
+function setColorContrastType(colorContrastType) {
+	_colorContrastType=colorContrastType;
+	if (!selectedElement) return;
+
+	switch(_colorContrastType) {
+		case 'text-color':
+			contrastColor = colorForElement(_selectedElement,colorTypeToStyle['text-color']);
+			break;
+		case 'text-border':
+			contrastColor = colorForElement(_selectedElement,colorTypeToStyle['-webkit-text-stroke-color']);
+			break;
+		case 'element-border':
+			contrastColor = colorForElement(_selectedElement,colorTypeToStyle['border-color']);
+			break;
 	}
-} );
+}
+
+function setMarkerGap(markerGap) {
+	_markerGap=markerGap;
+}
+
+function getOffset(el) {
+	const rect = el.getBoundingClientRect();
+	return {
+	  left: rect.left + window.scrollX,
+	  top: rect.top + window.scrollY
+	};
+  }
+  
+
+function play() {
+	shouldRunContrast=1;
+	const { element: parentBgElement, backgroundColor, backgroundImage } = colorContrastType !=='element-border' ?
+	findBgElement(_selectedElement) : findBgElement(_selectedElement.parentElement);
+    canvas = document.createElement('canvas');
+	const context = canvas.getContext('2d');
+
+	const sourceWidth = parentBgElement.clientWidth;
+    const sourceHeight = parentBgElement.clientHeight;
+
+	canvas.width = sourceWidth;
+    canvas.height = sourceHeight;
+    canvas.style.height = sourceHeight + "px";
+    canvas.style.width = sourceWidth + "px";
+	if (backgroundColor) {
+	 	canvas.style.backgroundColor = backgroundColor;
+	} else {
+		canvas.style.backgroundImage = backgroundImage;
+	}
+	// const { left, top } = getOffset(parentBgElement);
+}
+
+function reset() {
+	shouldRunContrast=0;
+}
+
+chrome.runtime.onMessage.addListener(function ( message ) {
+	switch(message.type) {
+		case PANEL_INIT: 
+			chrome.runtime.sendMessage( { type: PANEL_REGISTER_FRAME, url: window.location.href } );
+			break;
+		case SET_COLOR_CONTRAST:
+			setColorContrastType(message.value);
+			chrome.runtime.sendMessage( { type: UPDATE_CONTRAST_COLOR, color: contrastColor } );
+			break;
+		case SET_MARKER_GAP:
+			setMarkerGap(message.value)
+			break;
+		case UPDATE_SHOULD_RUN_CONTRAST:
+			const { value } = message;
+			if (value===true) {
+				play();
+			} else {
+				reset();
+			}
+			break;
+	}
+});
--- js/devtools.js
+++ js/devtools.js
@@ -2,6 +2,21 @@ chrome.devtools.panels.create("A11y Gradient", "../assets/logo/icon16.png", "htm
 
 const evalString = "$0.style.backgroundColor = 'red'";
 
+const A11Y_CONTEXT = 'A11Y_CONTEXT';
+const PANEL_INIT = `${A11Y_CONTEXT}_PANEL_INIT`;
+const PANEL_REGISTER_FRAME = `${A11Y_CONTEXT}_REGISTER_FRAME`;
+const UPDATE_SELECTED_ELEMENT = `${A11Y_CONTEXT}_UPDATE_SELECTED_ELEMENT`;
+const SET_COLOR_CONTRAST = `${A11Y_CONTEXT}_SET_COLOR_CONTRAST`;
+const UPDATE_CONTRAST_COLOR = `${A11Y_CONTEXT}_UPDATE_CONTRAST_COLOR`;
+const SET_MARKER_GAP = `${A11Y_CONTEXT}_SET_MARKER_GAP`;
+const UPDATE_SHOULD_RUN_CONTRAST = `${A11Y_CONTEXT}_UPDATE_SHOULD_RUN_CONTRAST`;
+let tabId;
+
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    console.log('tabId',tabs[0].id);
+    tabId=tabs[0].id;
+});
+
 function handleError(error) {
   if (error.isError) {
     console.log(`Devtools error: ${error.code}`);
@@ -10,43 +25,64 @@ function handleError(error) {
   }
 }
 
-let tabId;
-
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    console.log('tabId',tabs[0].id);
-    tabId=tabs[0].id;
-});
 
 function colorBorder (value) {
     console.log('qwwe',value);
     document.body.style.border = `${value}px solid green`;
 }
 
-async function handleMarker(e) {
-    const { value } = e.target;
-    console.log('val',value);
-    console.log('chrome',chrome);
-    //console.log('browser',browser);
-    chrome.scripting.executeScript({
-          target: {
-            tabId,
-          },
-          args: [value],
-          func: colorBorder
-},(res,error) => { 
-    console.log('err',error);
- });
+function updateElementSelected(node) {
+    const elementDetails = document.querySelector('.element-selected');
+    const closingTagIndex = node.indexOf('>');
+    elementDetails.querySelector('summary').innerText=node.substr(0,closingTagIndex+1);
+    elementDetails.querySelector('p').innerText=node.substring(closingTagIndex+1);
+}
+
+function updateContrastColor(color) {
+    const container = document.querySelector(".contrast-color-display-container");
+    const displayBox = document.querySelector("#contrast-color-display");
+    const displayBoxLabel = document.querySelector("#contrast-color-display-label");
+    displayBox.style.backgroundColor  = color;
+    displayBoxLabel.innerText = color;
+    container.style.display = 'flex';
+}
+
+function handleChangeContrastAgainst(e) {
+    port.postMessage( { type: SET_COLOR_CONTRAST, value: e.target.value } );
+}
+
+function handleReset(e) {
+    port.postMessage( { type: UPDATE_SHOULD_RUN_CONTRAST, value: false } );
+}
+
+function handlePlay(e) {
+    port.postMessage( { type: UPDATE_SHOULD_RUN_CONTRAST, value: true } );
 }
 
 document.querySelector('.marker-input').addEventListener('change',handleMarker);
+document.querySelector('#color-contrast-radioContainer').addEventListener('change', handleChangeContrastAgainst);
+document.querySelector('.reset').addEventListener('click',handleReset);
+document.querySelector('.play').addEventListener('click',handlePlay);
 
-const A11Y_CONTEXT = 'A11Y_CONTEXT';
-const PANEL_INIT = `${A11Y_CONTEXT}_PANEL_INIT`;
-const PANEL_REGISTER_FRAME = `${A11Y_CONTEXT}_REGISTER_FRAME`;
-const UPDATE_SELECTED_ELEMENT = `${A11Y_CONTEXT}_UPDATE_SELECTED_ELEMENT`;
+async function handleMarker(e) {
+    const { value } = e.target;
+    port.postMessage( { type: SET_MARKER_GAP, value } );
+//     console.log('val',value);
+//     console.log('chrome',chrome);
+
+//     //console.log('browser',browser);
+//     chrome.scripting.executeScript({
+//           target: {
+//             tabId,
+//           },
+//           args: [value],
+//           func: colorBorder
+// },(res,error) => { 
+//     console.log('err',error);
+//  });
+}
 
 const port = chrome.runtime.connect( { name: A11Y_CONTEXT } );
-
 port.onMessage.addListener( function ( msg ) {
     switch (msg.type) {
         case PANEL_REGISTER_FRAME: {
@@ -66,9 +102,17 @@ port.onMessage.addListener( function ( msg ) {
             } );
             break;
         }
+
         case UPDATE_SELECTED_ELEMENT: {
-            const { selectedElement } = msg;
+            const { selectedElement, color } = msg;
             updateElementSelected(selectedElement);
+            updateContrastColor(color);
+            break;
+        }
+
+        case UPDATE_CONTRAST_COLOR:  {
+            const { color  } = msg;
+            updateContrastColor(color);
             break;
         }
     }
@@ -76,10 +120,3 @@ port.onMessage.addListener( function ( msg ) {
 
 // Announce to content.js that they should register with their frame urls.
 port.postMessage( { type: PANEL_INIT } );
-
-function updateElementSelected(node) {
-    const elementDetails = document.querySelector('.element-selected');
-    const closingTagIndex = node.indexOf('>');
-    elementDetails.querySelector('summary').innerText=node.substr(0,closingTagIndex+1);
-    elementDetails.querySelector('p').innerText=node.substring(closingTagIndex+1);
-}
